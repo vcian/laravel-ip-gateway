aa98b0e2a55d8eff0a0d8997b38e16e0a92c7bff
Bharat khairnar <bharat.khairnar@viitor.cloud>
2023-06-19 15:19:02 +0530

    Add try, catch block into the code

--- src/Middleware/IpGatewayMiddleware.php
+++ src/Middleware/IpGatewayMiddleware.php
@@ -27,28 +27,34 @@ class IpGatewayMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $prohibitRequest = false;
-        $getClientIps = $request->getClientIps();
-        $enableBlacklist = config('ip-gateway.enable_blacklist') === true;
-        $redirectRoute = config('ip-gateway.redirect_route_to');
+        try {
+            $prohibitRequest = false;
+            $getClientIps = $request->getClientIps();
+            $enableBlacklist = config('ip-gateway.enable_blacklist') === true;
+            $redirectRoute = config('ip-gateway.redirect_route_to');
 
-        if (config('ip-gateway') && config('ip-gateway.enable_package') === true) {
-            foreach ($getClientIps as $ip) {
-                if ($enableBlacklist && $this->grantIpAddress($ip)) { // Its check blacklisted ip-addresses from ip-config file.
-                    $prohibitRequest = true;
-                    Log::warning($ip . ' IP address has tried to access.');
-                } elseif (!$enableBlacklist && !$this->grantIpAddress($ip)) { // Its check whitelisted ip-addresses from ip-config file.
-                    $prohibitRequest = true;
-                    Log::warning($ip . ' IP address has tried to access.');
+            if (config('ip-gateway') && config('ip-gateway.enable_package') === true) {
+                foreach ($getClientIps as $ip) {
+                    if ($enableBlacklist && $this->grantIpAddress($ip)) { // Its check blacklisted ip-addresses from ip-config file.
+                        $prohibitRequest = true;
+                        Log::warning($ip . ' IP address has tried to access.');
+                    } elseif (!$enableBlacklist && !$this->grantIpAddress($ip)) { // Its check whitelisted ip-addresses from ip-config file.
+                        $prohibitRequest = true;
+                        Log::warning($ip . ' IP address has tried to access.');
+                    }
                 }
             }
+
+            if ($prohibitRequest) {
+                return redirect($redirectRoute != '' ? $redirectRoute : '/404');
+            }
+
+            return $next($request);
+
+        } catch (\Throwable $th) {
+            Log::warning('Problem occurred while handle an incoming request '.$th->getMessage());
         }
 
-        if ($prohibitRequest) {
-            return redirect($redirectRoute != '' ? $redirectRoute : '/404');
-        }
-
-        return $next($request);
     }
 
     /**
